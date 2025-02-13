7de83346e615ad4e86e40b7bef51f335eb979cda
gaviash <71966054+gaviash@users.noreply.github.com>
2025-02-13 12:25:39 +0100

    Structure Basique

--- Scripts/oizo.gd
+++ Scripts/oizo.gd
@@ -1,6 +1,25 @@
-extends CharacterBody2D
-#MIAM
-#commit and add test
+extends CharacterBody2D 
+var launched = false
+
 func _physics_process(delta: float):
-	
-	move_and_slide()
+	if Input.is_action_just_pressed("ui_accept"):
+		launched = true
+	velocity = Vector2.ZERO
+	if launched :
+		print("chef")
+		coherence()
+		alignement()
+		separation()
+		
+	#move_and_slide() #Je le mettrai quand j'aurais fait apparaitre les oiseaux en aleatoire
+
+#Fonctions a coder de Boids :
+
+func coherence():
+	pass
+
+func alignement():
+	pass
+
+func separation():
+	pass
