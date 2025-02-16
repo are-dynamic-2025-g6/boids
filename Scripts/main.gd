32e6f5c4a120a6ff9c88b8b796b3b18a79878e54
gaviash <71966054+gaviash@users.noreply.github.com>
2025-02-16 17:59:54 +0100

    Fondations : Dictionnaire de distances

--- Scripts/main.gd
+++ Scripts/main.gd
@@ -1,10 +1,51 @@
 extends Node2D
+@onready var refresh_rate: Timer = $refresh_rate
 var oizo : PackedScene = preload("res://Scenes/oizo.tscn")
-
+var cage : Array[Node]
+var dico_distances : Array
+var curr_dist : float
 
 func _ready() -> void:
+	#gere les doublons au spawn
 	for i in range(50):
 		var oizoo : CharacterBody2D = oizo.instantiate()
-		oizoo.position = Vector2(300,200) #coordonnées aleatoires
+		oizoo.position = Vector2(randi_range(0,1140),randi_range(0,640)) #coordonnées aleatoires
+		oizoo.self_index = i
 		print(oizoo.position)
 		add_child(oizoo)
+		dico_distances.append({})
+		
+	cage = get_tree().get_nodes_in_group("Zoizos")
+	refresh_rate.start()
+
+
+
+func _process(delta: float) -> void:
+	#if Input.is_action_just_pressed("print"):
+	#	print(dico_distances)
+	pass
+
+
+func _on_timer_timeout() -> void:
+	get_dico_distances()
+
+
+func get_dico_distances():
+	"""
+	Met a jour la liste de dictionnaires,de la forme :
+	dico_distances = List[dict[(i,k):distance] -> c'est a dire que chaque oiseau,de par son indice,possede un
+	dictionnaire entier de ses distances avec les autres.Pour avoir ce dictionnaire : dico_distance[i] avec i
+	l'indice de l'oiseau dans la liste cage.
+	"""
+	
+	for i in range(len(cage)):
+		for k in range(i+1,len(cage)):
+			curr_dist = distance(cage[i],cage[k])
+			dico_distances[i][Vector2(i,k)] = curr_dist
+			dico_distances[k][Vector2(k,i)] = curr_dist
+			#dico_distances[Vector2(i,k)] = distance(cage[i],cage[k])
+
+
+
+func distance(obj1 : CharacterBody2D,obj2 : CharacterBody2D) -> float :
+	return ((obj1.global_position.x-obj2.global_position.x)**2 + (obj1.global_position.y - obj2.global_position.y)**2)**0.5
--- Scripts/oizo.gd
+++ Scripts/oizo.gd
@@ -1,19 +1,34 @@
 extends CharacterBody2D 
 var launched = false
+@onready var refresh_rate : Timer = get_parent().get_node("refresh_rate")
+@onready var main : Node2D = get_parent()
+var self_index : int  #index personnel dans la liste "cage" et dans le dictionnaire des distances
+var dico_distances : Array
+
+func _ready() -> void:
+	refresh_rate.connect("timeout",boids)
+	velocity = Vector2.ZERO #met a zero tout les deplacements
 
 func _physics_process(delta: float):
 	if Input.is_action_just_pressed("ui_accept"):
 		launched = true
-	velocity = Vector2.ZERO
+	
+
+	move_and_slide()
+
+
+#Fonctions a coder de Boids :
+
+func boids(): #Fonction appelée toutes les 0.3 secondes (temps modulable)
+	dico_distances = main.dico_distances
 	if launched :
-		print("chef")
+		print(dico_distances[self_index])
 		coherence()
 		alignement()
 		separation()
 		
-	#move_and_slide() #Je le mettrai quand j'aurais fait apparaitre les oiseaux en aleatoire
+	pass
 
-#Fonctions a coder de Boids :
 
 func coherence():
 	pass
