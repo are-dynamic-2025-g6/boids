1c347ef035d4f6b05b9957bf8ffb53c53754e6cb
gaviash <71966054+gaviash@users.noreply.github.com>
2025-02-19 00:06:08 +0100

    fonction separation + mise en place d'un deplacement basique

--- Scripts/oizo.gd
+++ Scripts/oizo.gd
@@ -1,10 +1,10 @@
 extends CharacterBody2D 
 #Constantes experimentales
 
-const VISION_SEPARATION = 20
-const VISION_ALIGNEMENT = 40
-const VISION_COHESION = 100
-const AVOID_FACTOR = 0.05
+const VISION_SEPARATION = 50
+const VISION_ALIGNEMENT = 100
+const VISION_COHESION = 160
+const AVOID_FACTOR = 0.4
 var launched = false
 
 @onready var refresh_rate : Timer = get_parent().get_node("refresh_rate")
@@ -12,13 +12,18 @@ var launched = false
 @onready var cage : Array #Contient les listes des oiseaux,c'est par cette liste qu'on peut les manipuler
 var self_index : int  #index personnel dans la liste "cage" et dans le dictionnaire des distances
 var dico_distances : Array
+var curr_velo : Vector2
+var normaliseur : int
 
 func _ready() -> void:
-	refresh_rate.connect("timeout",boids.bind(get_physics_process_delta_time()))
-	velocity = Vector2.ZERO #met a zero tout les deplacements
+	refresh_rate.connect("timeout",boids)
+	#Definir une velocité de depart aleatoire :
+	curr_velo = Vector2(1000,0)  
 
 func _physics_process(delta: float):
-
+	#velocity.x = 200 * delta #mouvement basique de gauche a droite,qui sera remplacé par la ligne d'en dessous
+	velocity = curr_velo * delta   #Curr_velo sera notre vecteur direction a modifier
+	
 	if Input.is_action_just_pressed("ui_accept"):
 		cage = main.cage
 		launched = true
@@ -29,19 +34,15 @@ func _physics_process(delta: float):
 
 #Fonctions a coder de Boids :
 
-func boids(delta): #Fonction appelée toutes les 0.3 secondes (temps modulable)
-	print(delta)
-	velocity.x = 200 * delta
+func boids():
+	normaliseur = 1
 	dico_distances = main.dico_distances
-	#3 Listes
-	
-	#miam
 	if launched :
 		coherence()
 		alignement()
-		separation()
+		normaliseur = normaliseur + 1 if separation() else normaliseur 
+		curr_velo  = curr_velo / normaliseur
 		
-	pass
 
 
 func coherence():
@@ -55,17 +56,16 @@ func separation():
 	var proche_x = 0
 	var proche_y = 0
 	var oiz = boids_in_range(VISION_SEPARATION)
-	#CHANGEMENT
 	if oiz:
+		print(oiz)
 		for i in oiz:
 			proche_x += (position.x - cage[i].position.x)
 			proche_y += (position.y - cage[i].position.y)
 		curr_velo += Vector2(curr_velo.x + proche_x*AVOID_FACTOR ,curr_velo.y + proche_y*AVOID_FACTOR)
-		return True
+		return true
 	else:
-		return False
-		
-	
+		return false
+
 
 func boids_in_range(range : int):
 	var res : Array = []
